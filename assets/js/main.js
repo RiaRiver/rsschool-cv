13285ca9526f4d3aeee9bb78a36257d8352acdbe
Ria River <riria.river@gmail.com>
2023-07-03 21:02:18 +0300

    feat: add nav and header with styles

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  Particles.init({
+    selector: '.background',
+  });
+};
+
+const primary = '#04cfd8';
+const secondary = '#f2035b';
+const light = '#ffe6ef';
+
+const particles = Particles.init({
+  selector: '.background',
+  color: [primary, secondary, '#000000'],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: [light, primary, secondary],
+        maxParticles: 43,
+        connectParticles: false,
+      },
+    },
+  ],
+});
